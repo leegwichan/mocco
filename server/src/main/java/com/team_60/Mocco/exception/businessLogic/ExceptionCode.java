12f297b02df9ca2e1dbfa528fdbab38fc48e886f
doyeon00 <oab1998@naver.com>
2022-09-16 09:37:31 +0900

    feature : 잘못된 로그인 시 에러 메세지 전송 기능

--- server/src/main/java/com/team_60/Mocco/exception/businessLogic/ExceptionCode.java
+++ server/src/main/java/com/team_60/Mocco/exception/businessLogic/ExceptionCode.java
@@ -10,7 +10,14 @@ public enum ExceptionCode {
     EMAIL_NOT_EXIST(400, "해당 메일을 이용하는 사용자가 없습니다."),
     NICKNAME_ALREADY_EXIST(400, "해당 닉네임을 이용하는 사용자가 이미 존재합니다."),
     GITHUB_REPOSITORY_DUPLICATION(400, "중복된 github repository를 입력했습니다."),
-    NOT_GITHUB_REPOSITORY(400, "잘못된 github repository 입니다.");
+    NOT_GITHUB_REPOSITORY(400, "잘못된 github repository 입니다."),
+    USERNAME_NOT_FOUND(401,"해당하는 유저를 찾을 수 없습니다."),
+    CLAIM_NOT_EXIST(403,"권한 정보가 없는 토큰입니다."),
+    TOKEN_EXPIRED_EXCEPTION(403,"이미 만료된 토큰입니다."),
+    FAIL_DECODE_TOKEN(403,"토큰 검증에 실패하였습니다."),
+    BAD_REFRESH_TOKEN(403,"Refresh Token 정보가 일치하지 않습니다."),
+    BAD_REQUEST(403,"잘못된 요청입니다."),
+    BAD_REQUEST_TOKEN(403,"JWT 토큰이 잘못되었습니다.");
 
     @Getter
     private int status;
