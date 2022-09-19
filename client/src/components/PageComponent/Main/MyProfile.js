f8881e5a0920b33f0a1f4bc34d1be83878d54607
dawnheee <sherlock130815@gmail.com>
2022-09-19 22:24:23 +0900

    feat:mypage 레이아웃, 스타일 작업 / components main 폴더에 components 파일 생성하여 pages 폴더의 main에서 사용, eslint props 타입 오류로 eslint rules 추가

--- client/src/components/PageComponent/Main/MyProfile.js
+++ client/src/components/PageComponent/Main/MyProfile.js
@@ -1,16 +1,35 @@
 import React from 'react'; // eslint-disable-line no-unused-vars
 import { css } from '@emotion/react';
 
-const roundImg = css`
-  border: solid 1px black;
+const container = css`
+  width: 15%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
 `;
-function MyProfile() {
+const roundImg = css`
+  border-radius: 100%;
+  max-width: 80%;
+`;
+const name = css`
+  font-size: 20px;
+`;
+function MyProfile({ img, nickname, evaluation, githubId }) {
   return (
-    <section>
-      <div>사진</div>
-      <div css={roundImg}>닉네임</div>
-      <div>별</div>
-      <div>깃헙버튼</div>
+    <section css={container}>
+      <img
+        src={
+          img === null
+            ? 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQjYmlp9JDeNMaFZzw9S3G1dVztGqF_2vq9nA&usqp=CAU'
+            : 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQjYmlp9JDeNMaFZzw9S3G1dVztGqF_2vq9nA&usqp=CAU'
+        }
+        alt={'프로필 이미지'}
+        css={roundImg}
+      />
+      <div css={name}>{nickname}</div>
+      <div>평점{evaluation}</div>
+      <button>깃헙버튼{githubId}</button>
     </section>
   );
 }
