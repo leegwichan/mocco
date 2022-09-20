5dccf2e360d04ca9f7c22169fd410212b1e804c3
dldnjswns31 <dldnjswns32@gmail.com>
2022-09-20 15:21:06 +0900

    style: header와 main 레이아웃 작업

--- client/src/pages/MakeStudy.js
+++ client/src/pages/MakeStudy.js
@@ -1,5 +1,24 @@
+import React from 'react'; // eslint-disable-line no-unused-vars
+import { css } from '@emotion/react';
+
+const Header = css`
+  width: 100%;
+  height: 64px;
+  background-color: rgba(0, 0, 0, 0.5);
+`;
+
+const MainContainer = css`
+  width: 100%;
+  height: calc(100vh - 64px);
+`;
+
 function MakeStudy() {
-  return <div>MakeStudy</div>;
+  return (
+    <>
+      <header css={Header}></header>
+      <main css={MainContainer}></main>
+    </>
+  );
 }
 
 export default MakeStudy;
