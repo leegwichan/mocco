9cbab0da58d870ce8049cd80bac4ede189e42c29
dldnjswns31 <dldnjswns32@gmail.com>
2022-09-23 05:51:39 +0900

    feature: MakeStudy layout 3차

--- client/src/pages/MakeStudy.js
+++ client/src/pages/MakeStudy.js
@@ -77,10 +77,11 @@ const SummaryTextArea = css`
 
 const IntroduceAndRulesContainer = css`
   width: 100%;
-  margin-top: 60px;
+  margin: 60px 0;
 `;
 
 const BigLabel = css`
+  display: inline-block;
   border-bottom: 3px solid #0b6ff2;
   font-size: 2rem;
 `;
@@ -183,6 +184,162 @@ function MakeStudy() {
                   </label>
                   <textarea type="text" name="rules" css={BigTextArea} />
                 </div>
+              </div>
+              <div
+                css={css`
+                  margin-top: 30px;
+                  margin-bottom: 120px;
+                `}
+              >
+                <div
+                  css={css`
+                    display: flex;
+                    margin-bottom: 2rem;
+                    justify-content: space-between;
+                    align-items: center;
+                  `}
+                >
+                  <div css={BigLabel}>스터디 TASK</div>
+                  <button
+                    css={css`
+                      height: 40px;
+                      padding: 0 1rem;
+                      color: white;
+                      font-size: 1.1rem;
+                      border: none;
+                      border-radius: 8px;
+                      background-color: #0b6ff2;
+                      transition: all 0.1s linear;
+                      &:hover {
+                        color: #0b6ff2;
+                        background-color: white;
+                        border: 1px solid #0b6ff2;
+                      }
+                    `}
+                  >
+                    TASK 추가하기
+                  </button>
+                </div>
+                {/* Task 목록 */}
+                <ul>
+                  <li
+                    css={css`
+                      display: flex;
+                    `}
+                  >
+                    <div
+                      css={css`
+                        display: flex;
+                        margin-right: 0.5rem;
+                        align-items: flex-start;
+                      `}
+                    >
+                      <button
+                        css={css`
+                          font-size: 20px;
+                          line-height: 40px;
+                          border: none;
+                          background-color: white;
+                        `}
+                      >
+                        ❌
+                      </button>
+                    </div>
+                    <div
+                      css={css`
+                        display: inline-block;
+                        height: 100%;
+                        margin-right: 1rem;
+                      `}
+                    >
+                      <span
+                        css={css`
+                          color: #0b7ff2;
+                          font-size: 40px;
+                        `}
+                      >
+                        Task 1
+                      </span>
+                    </div>
+                    <div
+                      css={css`
+                        display: inline-block;
+                        flex: 1 0;
+                      `}
+                    >
+                      <input
+                        type="text"
+                        css={css`
+                          display: block;
+                          width: 100%;
+                          height: 45px;
+                          margin-bottom: 10px;
+                          padding: 0 1rem;
+                          font-size: 1.2rem;
+                          border: 1px solid #999999;
+                          border-radius: 5px;
+                        `}
+                      />
+                      <input
+                        type="date"
+                        css={css`
+                          display: block;
+                          height: 44px;
+                          margin-top: 10px;
+                          padding: 0 1rem;
+                          font-size: 1.2rem;
+                          border: 1px solid #999999;
+                          border-radius: 5px;
+                        `}
+                      />
+                    </div>
+                  </li>
+                </ul>
+              </div>
+              <div
+                css={css`
+                  text-align: right;
+                `}
+              >
+                <button
+                  css={css`
+                    height: 40px;
+                    margin-right: 1rem;
+                    padding: 0 1rem;
+                    color: white;
+                    font-size: 1.1rem;
+                    border: none;
+                    border-radius: 8px;
+                    background-color: #999999;
+                    transition: all 0.1s linear;
+                    &:hover {
+                      color: #999999;
+                      background-color: white;
+                      border: 1px solid #999999;
+                    }
+                  `}
+                >
+                  취소
+                </button>
+                <button
+                  css={css`
+                    height: 40px;
+                    padding: 0 1rem;
+                    color: white;
+                    font-size: 1.1rem;
+                    border: none;
+                    border-radius: 8px;
+                    background-color: #0b6ff2;
+                    transition: all 0.1s linear;
+                    &:hover {
+                      color: #0b6ff2;
+                      background-color: white;
+                      border: 1px solid #0b6ff2;
+                    }
+                  `}
+                >
+                  스터디 만들기
+                </button>
               </div>
             </form>
           </section>
