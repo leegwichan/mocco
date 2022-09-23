f0d6d292b66e2c2cdca137a14ce35b4d3132d793
dawnheee <sherlock130815@gmail.com>
2022-09-23 19:35:03 +0900

    mypage 평가 기능 구현 중 오류로 렌더링이 안되어 문제되는 부분 주석처리해 렌더링 되는 상태. 이전 커밋과 똑같은 코드에서 오류

--- client/src/components/PageComponent/Main/MyStudyList/Carousel.js
+++ client/src/components/PageComponent/Main/MyStudyList/Carousel.js
@@ -49,9 +49,7 @@ function Carousel({ studyArr, status, clickHandler }) {
   useEffect(() => {
     const slideWidth = 250;
     const slideMargin = 100;
-    console.log(studyArr[0]);
-    console.log(studyArr[2]);
-    console.log(studyArr.length);
+    // console.log(studyArr.length);
     slides.current.style.width =
       (slideWidth + slideMargin) * length - slideMargin + 'px';
   }, []);
