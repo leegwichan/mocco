36b52b8ff972cd844dddd12debe72d242446979e
seraheo <heosera76@gmail.com>
2022-09-23 11:39:09 +0900

    style : 회원정보 수정 페이지 view 구현
    - ModifyUserInput component 생성
    - ModifyUserButton component 생성

---
+++ client/src/components/PageComponent/ModifyUser/ModifyUserButton.js
@@ -0,0 +1,46 @@
+import { css } from '@emotion/react';
+
+function ModifyUserButton({ buttonText, type }) {
+  return (
+    <div>
+      {buttonText !== '회원 탈퇴' && (
+        <button type={type} css={positiveButtonStyle}>
+          {buttonText}
+        </button>
+      )}
+      {buttonText === '회원 탈퇴' && (
+        <button type={type} css={negativeButtonStyle}>
+          {buttonText}
+        </button>
+      )}
+    </div>
+  );
+}
+
+const positiveButtonStyle = css`
+  width: 100%;
+  height: 40px;
+  color: #ffffff;
+  background-color: #0b6ff2;
+  border-radius: 5px;
+  border-width: 0px;
+  margin-top: 12px;
+  margin-bottom: 30px;
+  font-size: 18px;
+  font-weight: normal;
+`;
+
+const negativeButtonStyle = css`
+  width: 100%;
+  height: 40px;
+  color: #ffffff;
+  background-color: #999999;
+  border-radius: 5px;
+  border-width: 0px;
+  margin-top: 12px;
+  margin-bottom: 30px;
+  font-size: 18px;
+  font-weight: normal;
+`;
+
+export default ModifyUserButton;
--- client/src/pages/ModifyUser.js
+++ client/src/pages/ModifyUser.js
@@ -1,5 +1,36 @@
+import { css } from '@emotion/react';
+import ModifyUserInput from '../components/PageComponent/ModifyUser/ModifyUserInput';
+import ModifyUserButton from '../components/PageComponent/ModifyUser/ModifyUserButton';
+
 function ModifyUser() {
-  return <div>ModifyUser</div>;
+  return (
+    <div
+      css={css`
+        max-width: 350px;
+        margin: 0 auto;
+        padding: 100px 0px;
+      `}
+    >
+      <div
+        css={css`
+          margin-bottom: 18px;
+          border-bottom: 1px solid #d1d1d1;
+          padding-bottom: 18px;
+          font-size: 32px;
+        `}
+      >
+        <h3>회원정보 수정</h3>
+      </div>
+      <ModifyUserInput labelText="닉네임" type="text" />
+      <ModifyUserInput labelText="위치" type="text" />
+      <ModifyUserButton buttonText="비밀번호 수정" />
+      <ModifyUserInput labelText="이미지 추가 / 변경" type="textarea" />
+      <ModifyUserInput labelText="자기소개" type="textarea" />
+      <ModifyUserInput labelText="레포지토리" type="textarea" />
+      <ModifyUserButton buttonText="프로필 설정 완료" />
+      <ModifyUserButton buttonText="회원 탈퇴" />
+    </div>
+  );
 }
 
 export default ModifyUser;
