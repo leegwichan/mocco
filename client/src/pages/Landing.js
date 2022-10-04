95c1fb27df0455878f4b6847aba68bfef4cfaa4b
seraheo <heosera76@gmail.com>
2022-10-04 17:41:32 +0900

    fix: 로그인 로직 수정
    -로그인시 userInfo 안받아와지는 오류 수정

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,6 +1,10 @@
 import { css } from '@emotion/react';
+import { useRecoilValue } from 'recoil';
+import { userInfoState } from '../atom/atom';
 
 function Landing() {
+  const userInfo = useRecoilValue(userInfoState);
+  console.log('userInfo', userInfo);
   return (
     <div
       css={css`
