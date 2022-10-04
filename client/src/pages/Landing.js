f040b1274bfbe4d63776efc957d6b3eaeab8aacb
dawnheee <sherlock130815@gmail.com>
2022-10-04 17:46:17 +0900

    feature: 랜딩페이지 섹션 구획

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,13 +1,45 @@
 import { css } from '@emotion/react';
 
+const MainContainer = css`
+  width: 100vw;
+  padding-top: 8rem;
+  .second {
+    background-color: #f0f8ff;
+  }
+  .contentsContainer {
+    width: 90vw;
+    max-width: 1260px;
+    height: 90%;
+    border: 2px solid red;
+    display: flex;
+    justify-content: space-around;
+  }
+`;
+
+const seciton = css`
+  width: 100vw;
+  height: 50vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
 function Landing() {
   return (
-    <div
-      css={css`
-        margin-top: 70px;
-      `}
-    >
-      Landing
+    <div css={MainContainer}>
+      <section css={seciton}>
+        <div className="contentsContainer">
+          <div>이미지</div>
+          <div>글</div>
+        </div>
+      </section>
+      <section css={seciton} className="second">
+        <div className="contentsContainer">2</div>
+      </section>
+      <section css={seciton}>
+        <div className="contentsContainer">3</div>
+      </section>
     </div>
   );
 }
