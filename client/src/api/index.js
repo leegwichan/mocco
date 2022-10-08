e47d53c6f24d6e59258fcbef2b8169035ee9e056
seraheo <heosera76@gmail.com>
2022-10-09 00:55:59 +0900

    feature:  refreshToken 기능 구현

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import setAuthorizationToken from '../utils/setAuthorizationToken';
 
 const request = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
@@ -13,16 +14,34 @@ request.interceptors.response.use(
     return response;
   },
   async function (error) {
-    if (error.response && error.response.status === 403) {
-      const accessToken = localStorage.getItem('accessToken');
-      const refreshToken = localStorage.getItem('refreshToken');
-      const data = await axios({
+    if (
+      error.response &&
+      error.response.status === 403 &&
+      error.response.data.message === '이미 만료된 토큰입니다.'
+    ) {
+      const data = await request({
         method: 'post',
         url: '/api/register/refresh',
-        headers: { RefreshToken: refreshToken, AccessToken: accessToken },
+        headers: {
+          RefreshToken: localStorage.getItem('refreshToken'),
+          AccessToken: localStorage.getItem('accessToken'),
+        },
         // proxy: true,
+      }).then((res) => {
+        localStorage.setItem('accessToken', res.headers.accesstoken);
+        localStorage.setItem('refreshToken', res.headers.refreshtoken);
+        setAuthorizationToken(res.headers.accesstoken);
       });
-      console.log(data);
+
+      if (data?.isAxiosError) return Promise.reject(data);
+
+      return await request({
+        ...error.config,
+        headers: {
+          ...error.config.headers,
+          AccessToken: localStorage.getItem('accessToken'),
+        },
+      });
     }
     return Promise.reject(error);
   }
