2df2c571d0a8be23edddb28b0bd8c47646479d58
DESKTOP-E5VRH4Q\cnddk <cnddkscndgus@naver.com>
2022-10-10 22:21:50 +0900

    feature: 이메일로 새로 만든 비밀번호를 보낼 때, html 템플릿으로 보내게 수정

--- server/src/main/java/com/team_60/Mocco/helper/mail/sender/EmailSendable.java
+++ server/src/main/java/com/team_60/Mocco/helper/mail/sender/EmailSendable.java
@@ -1,9 +1,50 @@
 package com.team_60.Mocco.helper.mail.sender;
 
+import com.team_60.Mocco.member.entity.Member;
+import lombok.RequiredArgsConstructor;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Component;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 
 @Component
-public interface EmailSendable {
+@RequiredArgsConstructor
+public class EmailSendable {
 
-    void send(String[] to, String subject, String Message) throws InterruptedException;
+    private final JavaMailSender javaMailSender;
+    private final SpringTemplateEngine templateEngine;
+
+    public void sendSimpleEmail(String to, String subject, String message) throws InterruptedException {
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setTo(to);
+        mailMessage.setText(message);
+        mailMessage.setSubject(subject);
+        mailMessage.setText(message);
+        javaMailSender.send(mailMessage);
+
+        System.out.println("Sent simple email!");
+    }
+
+    public void sendTemplateEmailByChangingPassword(Member member, String subject, String newPassword) throws Exception {
+
+        MimeMessage message = javaMailSender.createMimeMessage();
+
+        message.addRecipients(MimeMessage.RecipientType.TO, member.getEmail());
+        message.setSubject(subject);
+        message.setText(setContext(member, newPassword), "utf-8", "html");
+        message.setFrom(new InternetAddress("mocco@gmail.com", "MOCCO"));
+
+        javaMailSender.send(message);
+    }
+
+    private String setContext(Member member, String newPassword){
+        Context context = new Context();
+        context.setVariable("nickname", member.getNickname());
+        context.setVariable("newPassword", newPassword);
+        return templateEngine.process("mail_password", context);
+    }
 }
