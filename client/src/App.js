3ae9cb67ccdd1bb55dbe8c4ac9ae23495b742c11
seraheo <heosera76@gmail.com>
2022-10-11 10:50:38 +0900

    fix: app.js 경로수정 Authentication

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import { useMemo } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { useRecoilValue } from 'recoil';
 import { userInfoState, preventAuthenticatedState } from './atom/atom';
-import PrivateRoute from './components/Common/authentication/PrivateRoute';
+import PrivateRoute from './components/Common/Authentication/PrivateRoute';
 import Landing from './pages/Landing';
 import SignUp from './pages/SignUp';
 import LogIn from './pages/LogIn';
