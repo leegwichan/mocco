59b6fa42291860bee04e791a0317c686b161c69d
leegwichan <44027393+leegwichan@users.noreply.github.com>
2022-10-27 11:54:40 +0900

    auto deploy test
    auto deploy test

--- server/script/deploy.sh
+++ server/script/deploy.sh
@@ -26,3 +26,4 @@ fi
 DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
 echo "> DEPLOY_JAR 배포"    >> /home/ubuntu/action/deploy.log
 sudo nohup java -jar --spring.profiles.active=dev $DEPLOY_JAR >> /home/ubuntu/deploy.log 2>/home/ubuntu/action/deploy_err.log &
+ 
